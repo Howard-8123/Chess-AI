d2cbc2fa29cdb5c334fe5e7af101a69e3ad700d8
Howard-8123 <lihongxi462@gmail.com>
2024-10-03 10:08:48 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from Classes import *
+from Class import *
 
 
 def main():
@@ -7,8 +7,24 @@ def main():
 
 def create_board():
     myboard = ChessBoard()
-    print(myboard.getboard())
-    
-
+    # white side
+    for x in range(0, 8):
+        for y in range(0, 2):
+            if y !=0:
+                if x == 0 or x ==8:
+                    myboard.change((x, y), Rook((x,y), 0))
+                elif x == 1 or x ==7:
+                    myboard.change((x, y), Knight((x, y), 0))
+                elif x ==2 or x == 6:
+                    myboard.change((x, y), Bishop((x, y), 0))
+                elif x == 3:
+                    myboard.change((x, y), Queen((x, y), 0))
+                elif x == 4:
+                    myboard.change((x, y), King((x, y), 0))
 
 create_board()
+
+def convert(np_array):
+    mytuple = tuple(np_array.tolist())
+    return mytuple
+
