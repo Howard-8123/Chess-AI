3f93073572df44d1dccbe997ff311ece29cab4eb
Howard-8123 <lihongxi462@gmail.com>
2024-10-10 12:59:35 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from Classes import *
+from chess import *
 
 
 def main():
@@ -12,24 +12,6 @@ def create_board():
         for y in range(0, 2):
             if y ==0:
                 if x == 0 or x ==7:
-                    piece = Rook((x,y), 0)
-                elif x == 1 or x ==6:
-                    piece = Knight((x,y), 0)
-                elif x ==2 or x == 5:
-                    piece = Bishop((x,y), 0)
-                elif x == 3:
-                    piece = Queen((x,y), 0)
-                elif x == 4:
-                    piece = King((x,y), 0)
-            else:
-                piece = Pawn((x, y), 0)
-            myboard.change((x, y), piece)
-        # Black side
-        for y in range(6, 8):
-            if y == 6:
-                piece = Pawn((x, y), 1)
-            else:
-                if x == 0 or x ==7:
                     piece = Rook((x,y), 1)
                 elif x == 1 or x ==6:
                     piece = Knight((x,y), 1)
@@ -39,6 +21,24 @@ def create_board():
                     piece = Queen((x,y), 1)
                 elif x == 4:
                     piece = King((x,y), 1)
+            else:
+                piece = Pawn((x, y), 1)
+            myboard.change((x, y), piece)
+        # Black side
+        for y in range(6, 8):
+            if y == 6:
+                piece = Pawn((x, y), 0)
+            else:
+                if x == 0 or x ==7:
+                    piece = Rook((x,y), 0)
+                elif x == 1 or x ==6:
+                    piece = Knight((x,y), 0)
+                elif x ==2 or x == 5:
+                    piece = Bishop((x,y), 0)
+                elif x == 3:
+                    piece = Queen((x,y), 0)
+                elif x == 4:
+                    piece = King((x,y), 0)
             myboard.change((x, y), piece)
     # print(myboard.getboard())
     # visualise = []
